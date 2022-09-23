acc4fb05475e42e7178214a3b89aef33a170d65a
kirka50 <75656501+kirka50@users.noreply.github.com>
2022-09-23 18:25:56 +0300

    ReznickovPy

---
+++ Front/neiron-app/src/Components/BarChart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Bar } from 'react-charts'
+
+const BarChart = () => {
+    return (
+        <Bar
+            height={400}
+            width={600}
+        />
+    );
+};
+
+export default BarChart;
